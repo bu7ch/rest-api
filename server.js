5b004937f0f44d1fb7473feed325206dd5b3c09f
Rodolphe <dutchr@live.fr>
2020-06-04 15:28:57 +0200

    ajouter index method with controller & routes

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ const app = express();
 const path = require("path");
 
 
+const postRoutes = require('./routes/post');
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, "public")));
 
+
+app.use("/api/post", postRoutes);
 
 app.listen(3000, ()=> {
   console.log(`[🚧 Listening...!]`)
